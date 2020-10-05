be73a77e62ebaa5bb4d33f8340fac71d023ce29e
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-05 05:54:33 +0530

    add login form

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,25 @@ import { Switch, Route } from "react-router-dom";
 
 import Landing from "./Landing";
 import Header from "./Header";
+import Login from "./Login";
 import useStyles from "../styles/AppStyles";
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Header />
       <Switch>
-        <Route path="/" exact render={() => <Landing />} />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <Header />
+              <Landing />
+            </>
+          )}
+        />
+        <Route path="/login" exact render={() => <Login />} />
         <Route render={() => <h1>404</h1>} />
       </Switch>
     </div>
