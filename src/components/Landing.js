62a665c86336e13b13201f950fc9c879bae40658
Juggernau9 <xbhavyatomar@gmail.com>
2020-10-06 20:23:54 +0530

    delete working with backend

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,8 +3,14 @@ import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import axios from "axios";
 
 import DeleteIcon from "@material-ui/icons/Delete";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
-// import "./Landing.css";
 import makeStyles from "../styles/LandingStyles";
 
 import {
@@ -13,14 +19,46 @@ import {
 } from "../contexts/images.context";
 import { TokenContext } from "../contexts/token.context";
 
+import useInputState from "../hooks/useInputState";
+
 function Landing() {
   const classes = makeStyles();
   const token = useContext(TokenContext);
   const images = useContext(ImagesContext);
   const dispatchImages = useContext(DispatchImagesContext);
+  const [deleteImage, setDeleteImage] = useState({});
+  const [password, setPassword, resetPassword] = useInputState("");
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleDeleteIcon = async (image) => {
+    setDeleteImage(image);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.post(
+        "/api/user/deletepost",
+        {
+          password: password,
+          uuid: deleteImage.uuid,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+    } catch (error) {}
+    setOpen(false);
+  };
   useEffect(() => {
-    const getProfile = async () => {
+    const getImages = async () => {
       const response = await axios.get("/api/user/images", {
         headers: {
           Authorization: "Bearer " + token,
@@ -28,22 +66,60 @@ function Landing() {
       });
       dispatchImages({ type: "update", data: response.data.images });
     };
-    getProfile();
+    getImages();
   }, []);
 
   const renderImages = () => {
     return images.map((image) => (
       <div className={classes.imageContainer}>
-        <DeleteIcon className={classes.DeleteIcon} />
+        <div
+          className={classes.deleteContainer}
+          onClick={() => handleDeleteIcon(image)}
+        >
+          <DeleteIcon className={classes.DeleteIcon} />
+        </div>
         <img className={classes.image} src={image.url} />
       </div>
     ));
   };
   console.log(images);
   return (
-    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-      <Masonry className={classes.masonry}>{renderImages()}</Masonry>
-    </ResponsiveMasonry>
+    <>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+        <Masonry className={classes.masonry}>{renderImages()}</Masonry>
+      </ResponsiveMasonry>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">{deleteImage.label}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter your password to confirm that you want to delete this
+            image
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={setPassword}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="default">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="secondary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
--- src/styles/LandingStyles.js
+++ src/styles/LandingStyles.js
@@ -8,11 +8,20 @@ export default makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     alignContent: "center",
+    "&:hover svg": {
+      opacity: "1",
+    },
   },
-  DeleteIcon: {
+  deleteContainer: {
     position: "absolute",
     right: "2.5rem",
     top: "1rem",
+    cursor: "pointer",
+  },
+  DeleteIcon: {
+    color: "red",
+    opacity: "0",
+    transition: "opacity .2s ease-in-out",
   },
   image: {
     width: "90%",
